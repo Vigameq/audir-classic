b736c59c8d32bf61d021644c30d4484d30e7dc2d
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2025-12-24 17:54:37 -0500

    Add user invite modal

--- src/app/pages/user-management/user-management.ts
+++ src/app/pages/user-management/user-management.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-management',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-management.html',
   styleUrl: './user-management.scss',
 })
 export class UserManagement {
+  protected showInviteModal = false;
 
+  protected openInvite(): void {
+    this.showInviteModal = true;
+  }
+
+  protected closeInvite(): void {
+    this.showInviteModal = false;
+  }
 }
