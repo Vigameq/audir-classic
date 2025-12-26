8e7afbb5b0fafbb3eb80dd4d3d25fbcc7dbef611
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2025-12-26 11:58:28 +0400

    Add negative response rules card

--- src/app/pages/nc-management/nc-management.ts
+++ src/app/pages/nc-management/nc-management.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ResponseService } from '../../services/response.service';
 
 @Component({
   selector: 'app-nc-management',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nc-management.html',
   styleUrl: './nc-management.scss',
 })
 export class NcManagement {
+  private readonly responseService = inject(ResponseService);
 
+  protected get responseNames(): string[] {
+    return this.responseService.responses().map((response) => response.name);
+  }
 }
