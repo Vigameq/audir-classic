92a4272cbfd1721117b9245273703154c3dc798d
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-04 22:20:32 +0530

    Add NC response fields in perform drawer

--- src/app/pages/nc-management/nc-management.ts
+++ src/app/pages/nc-management/nc-management.ts
@@ -11,6 +11,13 @@ import { NcRecord, NcService } from '../../services/nc.service';
 export class NcManagement {
   private readonly ncService = inject(NcService);
   protected activeRecord: NcRecord | null = null;
+  protected ncResponse = {
+    rootCause: '',
+    containmentAction: '',
+    correctiveAction: '',
+    preventiveAction: '',
+    evidenceFile: '',
+  };
 
   protected get ncRecords(): NcRecord[] {
     return this.ncService.records();
@@ -18,9 +25,27 @@ export class NcManagement {
 
   protected openRecord(record: NcRecord): void {
     this.activeRecord = record;
+    this.ncResponse = {
+      rootCause: '',
+      containmentAction: '',
+      correctiveAction: '',
+      preventiveAction: '',
+      evidenceFile: '',
+    };
   }
 
   protected closeRecord(): void {
     this.activeRecord = null;
+  }
+
+  protected onEvidenceSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+    this.ncResponse.evidenceFile = file ? file.name : '';
+    input.value = '';
+  }
+
+  protected submitNc(): void {
+    // no-op for now
   }
 }
