d0df5570d53224c3dcdd5604e9f7e523d168d678
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-27 02:52:28 -0500

    template

--- src/app/pages/templates/templates.ts
+++ src/app/pages/templates/templates.ts
@@ -183,6 +183,9 @@ export class Templates implements OnInit {
           this.editQuestions = [];
           this.editError = '';
         },
+        error: () => {
+          this.editError = 'Unable to save changes. Please try again.';
+        },
       });
   }
 }
